27188ca6d5deff2753e38d005ad3886f1c81d949
pink <2223749886@qq.com>
2022-08-01 16:37:39 +0800

    完成主界面的功能开发

--- assets/js/bast.js
+++ assets/js/bast.js
@@ -1,4 +1,20 @@
 // 每次调用ajsx是，会优先调用本函数
 $.ajaxPrefilter(function (options) {
-    options.url='http://www.liulongbin.top:3007'+options.url
-})
+  options.url = "http://www.liulongbin.top:3007" + options.url;
+
+  // 全局同一挂在complete函数
+  // 无论是否成功接受数据都会回调complete函数
+  options.complete = function (res) {
+    // console.log("shipai");
+    // console.log(res);
+    // 在complete回调函数中，可以使用res.responseJSON拿到服务器响应数据
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      // 强制清空token且强制跳转到登录页面
+      localStorage.removeItem("token");
+      location.href = "./login.html";
+    }
+  };
+});
---
+++ assets/js/index.js
@@ -0,0 +1,59 @@
+$(function () {
+  // 用户信息
+  getUserInfo();
+
+  // 点击退出按钮退出功能
+  $("#btnLogout").on("click", function () {
+    var layer = layui.layer;
+
+    // 提示用户是否退出
+
+    layer.confirm(
+      "确认是否退出登录？",
+      { icon: 3, title: "提示" },
+      function (index) {
+        //do something
+        // 退出回调函数
+        localStorage.removeItem("token");
+        // 重新跳转页面
+        location.href = "./login.html";
+
+        // 关闭询问框
+
+        layer.close(index);
+      }
+    );
+  });
+});
+
+// 获取用户基本信息
+function getUserInfo() {
+  $.ajax({
+    method: "get",
+    url: "/my/userinfo",
+    headers: {
+      Authorization: localStorage.getItem("token") || "",
+    },
+    success: function (res) {
+      if (res.status != 0) {
+        return layui.layer.msg("获取信息失败");
+      }
+      // 调用renderAvatar渲染用户头像
+      renderAvatar(res.data);
+    },
+  });
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+  var name = user.nickname || user.username;
+  $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+  if (user.user_pic !== null) {
+    $(".layui-nav-img").attr("src", user.user_pic).show();
+    $(".text-avatar").hide();
+  } else {
+    $(".layui-nav-img").hide();
+    var first = name[0].toUpperCase();
+    $(".text-avatar").html(first).show();
+  }
+}
